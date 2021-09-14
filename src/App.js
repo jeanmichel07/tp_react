b06fb53885121b7c3a3656ced8a80076eceb4256
jeanmichel07 <tombokely@gmail.com>
2021-09-14 18:55:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/about" exact component={About}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default App;
